0fa6b3f69cd15acb0e07856e59b3deb0582d1a26
Balazs Kelemen <kelbal@gmail.com>
2023-10-07 10:03:23 +0100

    first commit

---
+++ Thought.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+
+export function Thought(props) {
+  const { thought, removeThought } = props;
+
+  useEffect(() => {
+    const timesUp = setTimeout(() => {
+   removeThought(thought)
+    }, thought.expiresAt - Date.now());
+
+    return () => {
+      clearTimeout(timesUp);
+    };
+  }, [thought]);
+
+  const handleRemoveClick = () => {
+    removeThought(thought.id);
+  };
+
+  return (
+    <li className="Thought">
+      <button
+        aria-label="Remove thought"
+        className="remove-button"
+        onClick={handleRemoveClick}
+      >
+        &times;
+      </button>
+      <div className="text">{thought.text}</div>
+    </li>
+  );
+}
